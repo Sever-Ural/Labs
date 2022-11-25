31bc1038bd0ddb61d4212ff52178a8caf6b7dffd
Sever-Ural <serov.info220@gmil.com>
2022-11-25 20:09:02 +0500

    Commit lab1

--- lr1/Lab6.java
+++ lr1/Lab6.java
@@ -1,4 +1,20 @@
 package lr1;
 
+import java.util.Scanner;
 public class Lab6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Write first name: ");
+        String firts_name = in.nextLine();
+
+        System.out.println("Write second name: ");
+        String second_name = in.nextLine();
+
+        System.out.println("Write patronymic: ");
+        String patronymic = in.nextLine();
+
+        System.out.println("Hello: " + firts_name + " " + second_name + " " + patronymic);
+        in.close();
+    }
 }
