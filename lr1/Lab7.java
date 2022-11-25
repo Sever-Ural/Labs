6508c65f21a6f6e6651182a1c54b655c180ee578
Sever-Ural <serov.info220@gmil.com>
2022-11-25 20:17:44 +0500

    Commit lab1

--- lr1/Lab7.java
+++ lr1/Lab7.java
@@ -1,4 +1,18 @@
 package lr1;
 
+import java.util.Scanner;
 public class Lab7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Your name: ");
+        String name = in.nextLine();
+
+        System.out.println("Your age:");
+        int age = in.nextInt();
+
+        System.out.println("Your name: " + name);
+        System.out.println("Your age: " + age);
+        in.close();
+    }
 }
