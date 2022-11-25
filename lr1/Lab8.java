701dcf28427b08795c3973feed29bfa034c14966
Sever-Ural <serov.info220@gmil.com>
2022-11-25 20:27:29 +0500

    Commit lab1

--- lr1/Lab8.java
+++ lr1/Lab8.java
@@ -1,4 +1,20 @@
 package lr1;
 
+import java.util.Scanner;
 public class Lab8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Week day: ");
+        String week = in.nextLine();
+
+        System.out.println("Мonth :");
+        String month = in.nextLine();
+
+        System.out.println("Date: ");
+        int date = in.nextInt();
+
+        System.out.println(week + " " + month + " " + date);
+        in.close();
+    }
 }
