edbfdb762d789e3a54990dd518dd9e69e6d993a9
Sever-Ural <serov.info220@gmil.com>
2022-11-26 14:26:53 +0500

    Commit lab1

--- timus_lab/task_1000/Main.java
+++ timus_lab/task_1000/Main.java
@@ -1,8 +1,20 @@
 package timus_lab.task_1000;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        int c = a + b;
+
+        out.println(c);
+        out.flush();
     }
 }
--- timus_lab/task_1293/Main.java
+++ timus_lab/task_1293/Main.java
@@ -1,8 +1,22 @@
 package timus_lab.task_1293;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int N = in.nextInt();
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        int result = (N * (A*B)) * 2;
+
+        out.println(result);
+        out.flush();
+
     }
 }
