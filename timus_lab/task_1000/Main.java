703d6a5defea504ed6f230dd6a29f74f394ffa59
Sever-Ural <serov.info220@gmil.com>
2022-11-26 14:01:33 +0500

    Commit lab1

---
+++ timus_lab/task_1000/Main.java
@@ -0,0 +1,8 @@
+package timus_lab.task_1000;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ timus_lab/task_1293/Main.java
@@ -0,0 +1,8 @@
+package timus_lab.task_1293;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
