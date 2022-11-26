ccd36f1a3ae2534ca4c978317a5844ffa8eab63c
Sever-Ural <serov.info220@gmil.com>
2022-11-26 10:25:20 +0500

    Commit lab1

--- lr1/Lab13.java
+++ lr1/Lab13.java
@@ -1,4 +1,20 @@
 package lr1;
 
+import java.util.Scanner;
 public class Lab13 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("First number:");
+        int num1 = in.nextInt();
+
+        System.out.println("Second number:");
+        int num2 = in.nextInt();
+
+        num1 = num1 + num2;
+
+        System.out.println(num1);
+        in.close();
+    }
 }
