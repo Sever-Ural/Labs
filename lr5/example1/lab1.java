b101f49df01d0440400959e7f6e12d73302ae310
Sever-Ural <serov.info220@gmil.com>
2022-12-26 18:53:48 +0500

    Commit lab5

---
+++ lr5/example1/lab1.java
@@ -0,0 +1,18 @@
+package lr5.example1;
+
+class lab1 {
+    private char ch;
+
+    public void setCh(char ch) {
+        this.ch = ch;
+    }
+
+    public int getCh() {
+        return ch;
+    }
+    public void viewCode(){
+        System.out.println(ch);
+        int code = ch;
+        System.out.println(code);
+    }
+}
---
+++ lr5/example1/main.java
@@ -0,0 +1,11 @@
+package lr5.example1;
+
+public class main {
+    public static void main(String[] arg){
+        lab1 lab1 = new lab1();
+
+        lab1.setCh('A');
+        System.out.println(lab1.getCh());
+        lab1.viewCode();
+    }
+}
