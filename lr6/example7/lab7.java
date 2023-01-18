99898b60ae8ec72d5c021cee4754c1cee86bce21
Sever-Ural <serov.info220@gmil.com>
2023-01-18 17:02:59 +0500

    Commit lab6

---
+++ lr6/example7/lab7.java
@@ -0,0 +1,11 @@
+package lr6.example7;
+
+public class lab7 {
+    public static int[] arrayint(char [] c){
+        var temp = new int[c.length];
+        for(int i=0;i < c.length; i++){
+            temp[i]= c[i];
+        }
+        return temp;
+    }
+}
---
+++ lr6/example7/main.java
@@ -0,0 +1,9 @@
+package lr6.example7;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] arg){
+        System.out.println(Arrays.toString(lab7.arrayint(new char[]{'t','e','s','t'})));
+    }
+}
