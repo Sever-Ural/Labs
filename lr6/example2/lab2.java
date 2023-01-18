0f7c4ee2e62ee04241c4b5e6b3369a789b843a2d
Sever-Ural <serov.info220@gmil.com>
2023-01-18 15:38:32 +0500

    Commit lab6

---
+++ lr6/example2/lab2.java
@@ -0,0 +1,10 @@
+package lr6.example2;
+
+class lab2 {
+    static private int number;
+
+    static public void printNum(){
+        System.out.println(number);
+        number++;
+    }
+}
---
+++ lr6/example2/main.java
@@ -0,0 +1,8 @@
+package lr6.example2;
+
+public class main {
+    public static void main(String[]arg) {
+        for(int i=0;i<=5;i++)
+        lab2.printNum();
+    }
+}
